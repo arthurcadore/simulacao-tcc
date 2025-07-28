5fc891ebbae849acc3399ee2940d28eac3f339f4
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-07-28 15:10:58 -0300

    :memo: added coments to code

--- src/scrambler.py
+++ src/scrambler.py
@@ -3,7 +3,7 @@ from plots import Plotter
 
 class Scrambler:
     def __init__(self):
-        self.polynomial = [1, 0, 0, 0, 1, 0, 0, 1]
+        pass
 
     def scramble(self, X, Y):
         assert len(X) == len(Y), "Vetores X e Y devem ter o mesmo comprimento"
@@ -11,22 +11,24 @@ class Scrambler:
         Y_scrambled = []
 
         for i in range(0, len(X), 3):
-            # bloco de até 3 bits
             x_blk = X[i:i+3]
             y_blk = Y[i:i+3]
             n = len(x_blk)
 
             if n == 3:
+                # Embaralhamento do bloco [x1, x2, x3], [y1, y2, y3]
                 x1, x2, x3 = x_blk
                 y1, y2, y3 = y_blk
                 X_scrambled += [y1, x2, y2]
                 Y_scrambled += [x1, x3, y3]
             elif n == 2:
+                # Embaralhamento do bloco [x1, x2], [y1, y2]
                 x1, x2 = x_blk
                 y1, y2 = y_blk
                 X_scrambled += [y1, x2]
                 Y_scrambled += [x1, y2]
             elif n == 1:
+                # Embaralhamento do bloco [x1], [y1]
                 x1 = x_blk[0]
                 y1 = y_blk[0]
                 X_scrambled += [y1]
@@ -34,39 +36,34 @@ class Scrambler:
 
         return X_scrambled, Y_scrambled
 
-    def descramble(self, X_scrambled, Y_scrambled):
-        assert len(X_scrambled) == len(Y_scrambled), "Vetores devem ter o mesmo comprimento"
+    def descramble(self, X, Y):
+        assert len(X) == len(Y), "Vetores X e Y devem ter o mesmo comprimento"
         X_original = []
         Y_original = []
 
-        for i in range(0, len(X_scrambled), 3):
-            x_ = X_scrambled[i:i+3]
-            y_ = Y_scrambled[i:i+3]
-            n = len(x_)
+        for i in range(0, len(X), 3):
+            x_blk = X[i:i+3]
+            y_blk = Y[i:i+3]
+            n = len(x_blk)
 
             if n == 3:
-                # [y1, x2, y2], [x1, x3, y3]
-                x1 = y_[0]
-                x2 = x_[1]
-                x3 = y_[1]
-                y1 = x_[0]
-                y2 = x_[2]
-                y3 = y_[2]
-                X_original += [x1, x2, x3]
-                Y_original += [y1, y2, y3]
+                # Desembaralhamento do bloco [y1, x2, y2], [x1, x3, y3]
+                x1, x2, x3 = y_blk[0], x_blk[1], y_blk[1]
+                y1, y2, y3 = x_blk[0], x_blk[2], y_blk[2]
+                X_original.extend([x1, x2, x3])
+                Y_original.extend([y1, y2, y3])
             elif n == 2:
-                # [y1, x2], [x1, y2]
-                x1 = y_[0]
-                x2 = x_[1]
-                y1 = x_[0]
-                y2 = y_[1]
-                X_original += [x1, x2]
-                Y_original += [y1, y2]
+                # Desembaralhamento do bloco [y1, x2], [x1, y2]
+                x1, x2 = y_blk[0], x_blk[1]
+                y1, y2 = x_blk[0], y_blk[1]
+                X_original.extend([x1, x2])
+                Y_original.extend([y1, y2])
             elif n == 1:
-                x1 = y_[0]
-                y1 = x_[0]
-                X_original += [x1]
-                Y_original += [y1]
+                # Desembaralhamento do bloco [y1], [x1]
+                x1 = y_blk[0]
+                y1 = x_blk[0]
+                X_original.append(x1)
+                Y_original.append(y1)
 
         return X_original, Y_original
 
@@ -77,13 +74,26 @@ if __name__ == "__main__":
 
     vt0 = np.random.randint(0, 2, 30)
     vt1 = np.random.randint(0, 2, 30)
-    Xn, Yn = scrambler.scramble(vt0, vt1)
 
+    idx_vt0 = [f"X{i+1}" for i in range(len(vt0))]
+    idx_vt1 = [f"Y{i+1}" for i in range(len(vt1))]
+
+    # Embaralha o conteúdo dos vetores e os indices
+    Xn, Yn = scrambler.scramble(vt0, vt1)
+    idx_Xn, idx_Yn = scrambler.scramble(idx_vt0, idx_vt1)
+
+    print("\nSequência original:")
     print("vt0: ", ''.join(str(b) for b in vt0))
     print("vt1: ", ''.join(str(b) for b in vt1))
+    print("idx_vt0:", idx_vt0[:12])
+    print("idx_vt1:", idx_vt1[:12])
+
+    print("\nSequência embaralhada:")
     print("Xn  :", ''.join(str(int(b)) for b in Xn))
     print("Yn  :", ''.join(str(int(b)) for b in Yn))
-    
+    print("idx_Xn: ", idx_Xn[:12])
+    print("idx_Yn: ", idx_Yn[:12])
+
     vt0_prime, vt1_prime = scrambler.descramble(Xn, Yn)
 
     print("\nVerificação:")
@@ -92,7 +102,6 @@ if __name__ == "__main__":
     print("vt0 = vt0': ", np.array_equal(vt0, vt0_prime))
     print("vt1 = vt1': ", np.array_equal(vt1, vt1_prime))
 
-    
     plotter = Plotter()
     plotter.plot_scrambler(vt0, 
                            vt1, 
@@ -108,4 +117,4 @@ if __name__ == "__main__":
                            "Canal Q $v_t^{(1)'} $",
                            save_path="../out/example_scrambling.pdf"
     )
-    
+
