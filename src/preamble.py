b60b94bb29c841095ba5cf866e73e89bbc853d32
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-08-23 17:03:44 +0000

    :memo: updated preamble documentation

--- src/preamble.py
+++ src/preamble.py
@@ -15,17 +15,30 @@ class Preamble:
 
     def __init__(self, preamble_hex="2BEEEEBF"):
         r"""
-        Inicializa uma instância de palavra de sincronismo. A palavra de sincronismo $S$ é composta por 30 bits, $S = 2BEEEEBF_{16}$, conforme o padrão PPT-A3.
+        Inicializa uma instância de palavra de sincronismo, $S = 2BEEEEBF_{16}$ no padrão ARGOS-3. A palavra de sincronismo composta por 30 bits, $S = [S_0, S_1, S_2, \dots, S_{29}]$ que são intercalados para formar os vetores $S_I$ e $S_Q$ de cada canal, conforme apresentado abaixo.
 
-        Referência:
-            AS3-SP-516-274-CNES (seção 3.1.4.6)
+        $$
+        \begin{align}
+        S_I &= [S_0,\, S_2,\, S_4,\, \dots,\, S_{28}] && \mapsto \quad S_I = [1111,\, 1111,\, 1111,\, 111] \\
+        S_Q &= [S_1,\, S_3,\, S_5,\, \dots,\, S_{29}] && \mapsto \quad S_Q = [0011,\, 0101,\, 0100,\, 111]
+        \end{align}
+        $$
+
+        Sendo:
+            - $S$: Palavra de sincronismo original.
+            - $S_I$ e $S_Q$: Vetores de saida correspondentes aos canais I e Q, respectivamente.
 
         Args:
             preamble_hex (str, opcional): Hexadecimal da palavra de sincronismo.
         
         Raises:
-            ValueError: Se a palavra de sincronismo não contiver 30 bits.
-            ValueError: Se o hexadecimal não for válido ou não puder ser convertido para 30 bits
+            ValueError: Se a palavra de sincronismo $S$ tiver comprimento diferente de 8 caracteres. 
+            ValueError: Se o hexadecimal não for válido ou não puder ser convertido.
+
+        <div class="referencia">
+        <b>Referência:</b><br>
+        AS3-SP-516-274-CNES (seção 3.1.4.6)
+        </div>
         """
 
         if not isinstance(preamble_hex, str) or len(preamble_hex) != 8:
@@ -40,32 +53,11 @@ class Preamble:
         self.preamble_sI, self.preamble_sQ = self.generate_preamble()
 
     def hex_to_bits(self, hex_string):
-        r"""
-        Converte uma string hexadecimal em uma string de bits de 30 bits.
-
-        Args:
-            hex_string (str): String hexadecimal a ser convertida.
-
-        Returns:
-            bin_str (str): String de bits de 30 bits.
-        """
         return format(int(hex_string, 16), '032b')[2:] 
     
     def generate_preamble(self):
         r"""
-        Gera os vetores I e Q da palavra de sincronismo, com base no vetor $S$ passado no construtor.
-
-        Definição dos vetores:
-        $$
-        \begin{align}
-            S_I &= [S_0, S_2, S_4, \dots, S_{28}]
-        \end{align}
-        $$
-        $$
-        \begin{align}
-            S_Q &= [S_1, S_3, S_5, \dots, S_{29}]
-        \end{align}
-        $$
+        Gera os vetores $S_I$ e $S_Q$ da palavra de sincronismo, com base no vetor $S$ passado no construtor.
 
         Returns:
             tuple (np.ndarray, np.ndarray): Vetores $S_I$ e $S_Q$.
