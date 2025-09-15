07d9046628db38f6ffe47057d0e44d92b7fab45b
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-09-14 23:02:21 -0300

    :sparkles: updated pyproject and init.py

--- src/argos3/__init__.py
+++ src/argos3/__init__.py
@@ -1,6 +1,15 @@
-from .bersnr import *
+try:
+    from importlib import metadata as _metadata
+except ImportError:
+    import importlib_metadata as _metadata 
+
+try:
+    __version__ = _metadata.version("argos3")
+except _metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+# Main Components
 from .convolutional import *
-from .data import * 
 from .datagram import *
 from .detector import *
 from .encoder import *
@@ -9,11 +18,21 @@ from .lowpassfilter import *
 from .matchedfilter import *    
 from .modulator import *
 from .multiplexer import *
-from .noise import *
-from .plotter import *
 from .preamble import *
-from .receiver import *
 from .sampler import * 
 from .scrambler import *
 from .synchronizer import *
+
+# Channel
+from .noise import *
+
+# Transmitter
 from .transmitter import *
+
+# Receiver
+from .receiver import *
+
+# Extras
+from .bersnr import *
+from .data import * 
+from .plotter import *
