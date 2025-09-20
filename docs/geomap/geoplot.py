cc8b11383cd17e13438065937c48697870ef7511
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-09-20 12:22:03 -0300

    :sparkles: updated geoplot on main

--- docs/geomap/geoplot.py
+++ docs/geomap/geoplot.py
@@ -21,15 +21,6 @@ INPUT_DATA = "data/geoencoded.csv"
 OUTPUT_DATA = "../out/geoplot.pdf"
 GEOJSON_URL = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson"
 
-UFS = {
-    "Acre": "AC", "Alagoas": "AL", "Amapá": "AP", "Amazonas": "AM", "Bahia": "BA", "Ceará": "CE",
-    "Distrito Federal": "DF", "Espírito Santo": "ES", "Goiás": "GO", "Maranhão": "MA", "Mato Grosso": "MT",
-    "Mato Grosso do Sul": "MS", "Minas Gerais": "MG", "Pará": "PA", "Paraíba": "PB", "Paraná": "PR",
-    "Pernambuco": "PE", "Piauí": "PI", "Rio de Janeiro": "RJ", "Rio Grande do Norte": "RN",
-    "Rio Grande do Sul": "RS", "Rondônia": "RO", "Roraima": "RR", "Santa Catarina": "SC",
-    "São Paulo": "SP", "Sergipe": "SE", "Tocantins": "TO"
-}
-
 def import_df(input_data):
     r"""
     Importa o dataframe
