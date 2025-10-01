a6a48364ffa7d52772c18a3979ed54e6033381d0
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-10-01 15:38:10 -0300

    :sparkles: updated formatter

--- src/argos3/formatter.py
+++ src/argos3/formatter.py
@@ -1,8 +1,8 @@
 # """
-# Implementa um formatador de pulso para transmissão de sinais digitais. 
-
-# Autor: Arthur Cadore
-# Data: 28-07-2025
+# Implements a pulse modulator compatible with the PPT-A3 standard.
+#
+# Author: Arthur Cadore
+# Date: 28-07-2025
 # """
 
 import numpy as np
@@ -12,36 +12,66 @@ from .encoder import Encoder
 class Formatter:
     def __init__(self, alpha=0.8, fs=128_000, Rb=400, span=6, type="RRC", prefix_duration=0.082, channel=None, bits_per_symbol=1):
         r"""
-        Inicializa um formatador, utilizado preparar os símbolos para modulação.
+        Initializes a pulse modulator, with pulse response $g(t)$ used to prepare the symbols for modulation. The deployment of the pulse response $g(t)$ for each channel is illustrated in the figure below.
 
         ![pageplot](../assets/pulse_modulate.svg)
 
         Args:
-            alpha (float): Fator de roll-off do pulso RRC.
-            fs (int): Frequência de amostragem.
-            Rb (int): Taxa de bits.
-            span (int): Duração do pulso em termos de períodos de bit.
-            type (str): Tipo de pulso, atualmente apenas $RRC$ é suportado.
-            prefix_duration (int): Duração da portadora pura no inicio do vetor
-            channel (str): Canal a ser formatado, apenas $I$ e $Q$ são suportados.
-            bits_per_symbol (int): Número de bits por símbolo.
+            alpha (float): Roll-off factor of the RRC pulse.
+            fs (int): Sampling frequency.
+            Rb (int): Bit rate.
+            span (int): Pulse duration in terms of bit periods.
+            type (str): Type of pulse, currently only $RRC$ is supported.
+            prefix_duration (int): Duration of the pure carrier at the beginning of the vector
+            channel (str): Channel to be formatted, only $I$ and $Q$ are supported.
+            bits_per_symbol (int): Number of bits per symbol.
 
         Raises:
-            ValueError: Se o tipo de pulso não for suportado.
-            ValueError: Se o canal não for suportado.
+            ValueError: If the pulse type is not supported.
+            ValueError: If the channel is not supported.
 
-        Example: 
-            ![pageplot](assets/example_formatter_time.svg)
+        Examples: 
+            >>> import argos3
+            >>> import numpy as np 
+            >>> 
+            >>> X = np.random.randint(0, 2, 20)
+            >>> Y = np.random.randint(0, 2, 20)
+            >>>
+            >>> print(X)
+            [0 1 1 1 0 1 1 1 0 0 0 0 0 0 1 0 1 1 1 1]
+            >>> print(Y)
+            [0 0 0 1 0 1 1 0 0 1 0 0 0 1 0 0 1 1 0 1]
+            >>> 
+            >>> symbols_I = argos3.Encoder(method="NRZ").encode(X)
+            >>> symbols_Q = argos3.Encoder(method="NRZ").encode(Y)
+            >>> 
+            >>> formatter_I = argos3.Formatter(Rb=1000, type="RRC", channel="I", bits_per_symbol=1, prefix_duration=0.01)
+            >>> formatter_Q = argos3.Formatter(Rb=1000, type="RRC", channel="Q", bits_per_symbol=2, prefix_duration=0.01)
+            >>> 
+            >>> dI = formatter_I.apply_format(symbols_I, add_prefix=True)
+            >>> dQ = formatter_Q.apply_format(symbols_Q, add_prefix=True)
+            >>> 
+            >>> print(dI[:10])
+            [-0.07484442 -0.07466479 -0.07461029 -0.07454939 -0.07448207 -0.07440832
+             -0.07432813 -0.0742415  -0.07414841 -0.07404887]
+            >>> 
+            >>> print(dQ[:10])
+            [-0.09576597 -0.09565452 -0.09570885 -0.09574086 -0.09575048 -0.09573767
+             -0.09570238 -0.0956446  -0.09556429 -0.09546146]
+
+            - Bitstream Plot Example: ![pageplot](assets/example_formatter_time.svg)
 
         <div class="referencia">
-        <b>Referência:</b><br>
-        EEL7062 – Princípios de Sistemas de Comunicação, Richard Demo Souza (Pg. 55)
+        <b>Reference:</b><br>
+        EEL7062, Princípios de Sistemas de Comunicação, Richard Demo Souza (Pg. 55)
         </div>
         """
 
+        # verify channel input
         if channel not in ["I", "Q"]:
-            raise ValueError("Canal inválido. Use 'I' ou 'Q'.")
+            raise ValueError("Channel not supported. Use 'I' or 'Q'.")
         
+        # Attributes
         self.channel = channel
         self.prefix_duration = prefix_duration  
         self.alpha = alpha
@@ -53,18 +83,18 @@ class Formatter:
         self.span = span
         self.t_rc = np.linspace(-span * self.Tb, span * self.Tb, span * self.sps * 2)
 
+        # type mapping
         type_map = {
             "rrc": 0,
             "manchester": 1
         }
-
-
         type = type.lower()
         if type not in type_map:
             raise ValueError("Tipo de pulso inválido. Use 'RRC' ou 'Manchester'.")
         
         self.type = type_map[type]
 
+        # generate pulse response for each type
         if self.type == 0:  # RRC
             self.g = self.rrc_pulse()
         elif self.type == 1:  # Manchester
@@ -72,32 +102,35 @@ class Formatter:
 
     def rrc_pulse(self, shift=0.0):
         r"""
-        Gera o pulso Root Raised Cosine ($RRC$). O pulso $RRC$ no dominio do tempo é definido pela expressão abaixo.
+        Generates the Root Raised Cosine ($RRC$) pulse. The $RRC$ pulse in the time domain is defined by the expression below.
 
         $$
             g(t) = \frac{(1 - \alpha) sinc((1- \alpha) t / T_b) + \alpha (4/\pi) \cos(\pi (1 + \alpha) t / T_b)}{1 - (4 \alpha t / T_b)^2}
         $$
 
-        Sendo: 
-            - $g(t)$: Pulso formatador $RRC$ no dominio do tempo.
-            - $\alpha$: Fator de roll-off do pulso.
-            - $T_b$: Período de bit.
-            - $t$: Vetor de tempo.
+        Where: 
+            - $g(t)$: $RRC$ pulse in the time domain.
+            - $\alpha$: Roll-off factor of the pulse.
+            - $T_b$: Bit period.
+            - $t$: Time vector.
 
         Args: 
-            shift (float): Deslocamento no tempo.
+            shift (float): Time shift, used to shift the pulse in the time domain.
 
         Returns:
-           rc (np.ndarray): Pulso RRC normalizado.
+           rc (np.ndarray): Normalized RRC pulse.
 
-        Example: 
-            ![pageplot](assets/example_formatter_impulse.svg)
+        Examples: 
+            - Impulse Response Example: ![pageplot](assets/example_formatter_impulse.svg)
         """
         self.t_rc = np.array(self.t_rc, dtype=float)
-        # aplica deslocamento no tempo
+        # apply time shift
         t_shifted = self.t_rc - shift
 
+        # create the rc array
         rc = np.zeros_like(t_shifted)
+
+        # calculate the rc
         for i, ti in enumerate(t_shifted):
             if np.isclose(ti, 0.0):
                 rc[i] = 1.0 + self.alpha * (4/np.pi - 1)
@@ -112,64 +145,64 @@ class Formatter:
                 den = np.pi * ti * (1 - (4 * self.alpha * ti / self.Tb) ** 2) / self.Tb
                 rc[i] = num / den
 
-        # Normaliza energia para 1
+        # normalize energy to 1
         rc = rc / np.sqrt(np.sum(rc**2))
         return rc
     
     def manchester_pulse(self):
         r"""
-        Pulso Manchester como diferença de dois RRC simetricamente deslocados. 
+        Manchester pulse, defined as the difference of two RRC pulses symmetrically shifted, as shown in the expression below.
 
         $$
             g_{MAN}(t) = g_{RRC}(t + T_b/2) - g_{RRC}(t - T_b/2)
         $$
 
-        Sendo: 
-            - $g_{MAN}(t)$: Pulso formatador Manchester no dominio do tempo.
-            - $g_{RRC}(t)$: Pulso formatador $RRC$ no dominio do tempo.
-            - $T_b$: Período de bit.
-            - $t$: Vetor de tempo.
+        Where: 
+            - $g_{MAN}(t)$: Manchester pulse in the time domain.
+            - $g_{RRC}(t)$: RRC pulse in the time domain.
+            - $T_b$: Bit period.
+            - $t$: Time vector.
 
-        Example: 
-            ![pageplot](assets/example_formatter_impulse_man.svg)
+        Examples: 
+            - Impulse Response Example: ![pageplot](assets/example_formatter_impulse_man.svg)
         """
         g_left = -self.rrc_pulse(shift=self.Tb/2)
         g_right = +self.rrc_pulse(shift=-self.Tb/2)
         g = g_left + g_right
 
-        # Normaliza energia para 1
+        # normalize energy to 1
         g = g / np.sqrt(np.sum(g**2))
 
         return g, g_left, g_right
 
     def apply_format(self, symbols, add_prefix=True):
         r"""
-        Formata os símbolos de entrada usando o pulso inicializado. O processo de formatação é dado por: 
+        Applies the formatting process to the input symbols using the initialized pulse. Also adds a prefix to the signal if `add_prefix` is True. The formatting process is given by: 
 
         $$
            d(t) = \sum_{n} x[n] \cdot g(t - nT_b)
         $$
 
-        Sendo: 
-            - $d(t)$: Sinal formatado de saída.
-            - $x$: Vetor de símbolos de entrada.
-            - $g(t)$: Pulso formatador.
-            - $n$: Indice de bit.
-            - $T_b$: Período de bit.
+        Where: 
+            - $d(t)$: Formatted signal output.
+            - $x$: Input symbol vector.
+            - $g(t)$: Formatting pulse.
+            - $n$: Bit index.
+            - $T_b$: Bit period.
 
         Args:
-            symbols (np.ndarray): Vetor de símbolos a serem formatados.
+            symbols (np.ndarray): Input symbol vector.
         
         Returns:
-            out_symbols (np.ndarray): Vetor formatado com o pulso aplicado.
+            out_symbols (np.ndarray): Formatted symbol vector.
         """
 
-        # adiciona prefixo
+        # add prefix if required
         if add_prefix:
             symbols = self.add_prefix(symbols)
 
         pulse = self.g
-        # samples per symbol agora é por bit dividido pelo número de bits por símbolo
+        # samples per symbol (bits per symbol)
         sps = int(self.fs / (self.Rb / self.bits_per_symbol))
 
         upsampled = np.zeros(len(symbols) * sps)
@@ -180,56 +213,60 @@ class Formatter:
 
     def add_prefix(self, symbols):
         """
-        Adiciona um prefixo de portadora pura no inicio do sinal. Para o canal $I$, adiciona um vetor de simbolos $+1$, para o canal $Q$, adiciona um vetor de simbolos $0$, com duração de `prefix_duration`, pois ao aplicar o modulador `IQ` temos uma portadora pura no inicio do sinal, conforme a expressão abaixo: 
+        Adds a pure carrier prefix to the signal. For the $I$ channel, adds a vector of symbols $+1$, for the $Q$ channel, adds a vector of symbols $0$, with duration of `prefix_duration`. 
+        
+        Since applying the `IQ` modulator we have a pure carrier at the beginning of the signal, according to the expression below: 
 
         $$
             s(t) = 1(t) \cdot \cos(2\pi f_c t) - 0(t) \cdot \sin(2\pi f_c t) \mapsto s(t) = \cos(2\pi f_c t)
         $$
 
-        Sendo: 
-            - $s(t)$: Sinal modulado.
-            - $1(t)$ e $0(t)$: Prefixo de portadora pura.
-            - $f_c$: Frequência da portadora.
-            - $t$: Vetor de tempo.
+        Where: 
+            - $s(t)$: Modulated signal.
+            - $1(t)$ and $0(t)$: Pure carrier prefix.
+            - $f_c$: Carrier frequency.
+            - $t$: Time vector.
         
         Args:
-            symbols (np.ndarray): Vetor de símbolos a serem formatados.
+            symbols (np.ndarray): Symbol vector to be formatted.
         
         Returns:
-            symbols (np.ndarray): Vetor de símbolos com prefixo adicionado.
+            symbols (np.ndarray): Symbol vector with prefix added.
         """
+
+        # verify channel
         if self.channel == "I":
             carrier = np.ones(int(self.prefix_duration * self.Rb / self.bits_per_symbol))
         elif self.channel == "Q":
             carrier = np.zeros(int(self.prefix_duration * self.Rb / self.bits_per_symbol))
 
+        # add prefix
         symbols = np.concatenate([carrier, symbols])
         return symbols
 
 
 if __name__ == "__main__":
 
-    bitN = np.random.randint(0, 2, 20)
-    bitM = np.random.randint(0, 2, 20)
+    X = np.random.randint(0, 2, 20)
+    Y = np.random.randint(0, 2, 20)
 
     encoder_I = Encoder(method="NRZ")
     encoded_Q = Encoder(method="NRZ")
 
-    symbols_I = encoder_I.encode(bitN)
-    symbols_Q = encoded_Q.encode(bitM)
+    symbols_I = encoder_I.encode(X)
+    symbols_Q = encoded_Q.encode(Y)
     
-    formatterI = Formatter(alpha=0.8, fs=128_000, Rb=400, span=12, type="RRC", channel="I", bits_per_symbol=1, prefix_duration=0)
-    formatterQ = Formatter(alpha=0.8, fs=128_000, Rb=400, span=12, type="Manchester", channel="Q", bits_per_symbol=2, prefix_duration=0)
+    formatterI = Formatter(alpha=0.8, fs=128_000, Rb=1000, span=10, type="RRC", channel="I", bits_per_symbol=1, prefix_duration=0.01)
+    formatterQ = Formatter(alpha=0.8, fs=128_000, Rb=1000, span=10, type="Manchester", channel="Q", bits_per_symbol=2, prefix_duration=0.01)
     
-    dI1 = formatterI.apply_format(symbols_I)
-    dQ1 = formatterQ.apply_format(symbols_Q)
+    dI1 = formatterI.apply_format(symbols_I, add_prefix=True)
+    dQ1 = formatterQ.apply_format(symbols_Q, add_prefix=True)
     
     print("Xn:",  ' '.join(f"{x:+d}" for x in symbols_I[:10]))
     print("Yn:",  ' '.join(f"{y:+d}" for y in symbols_Q[:10]))
     print("dI:", ''.join(str(b) for b in dI1[:5]))
     print("dQ:", ''.join(str(b) for b in dQ1[:5]))
 
-    # Plotando a resposta ao impulso
     fig_impulse, grid_impulse = create_figure(1, 1, figsize=(16, 5))
 
     ImpulseResponsePlot(
@@ -240,15 +277,13 @@ if __name__ == "__main__":
         label=r"$g(t)$", 
         xlabel=r"Tempo ($ms$)", 
         ylabel="Amplitude", 
-        xlim=(-15, 15), 
+        xlim=(-5, 5), 
         amp_norm=True
     ).plot()
 
     fig_impulse.tight_layout()
     save_figure(fig_impulse, "example_formatter_impulse.pdf")
 
-
-    # Plotando a resposta ao impulso (man)
     fig_impulse_man, grid_impulse_man = create_figure(2, 1, figsize=(16, 9))
 
     ImpulseResponsePlot(
@@ -260,7 +295,7 @@ if __name__ == "__main__":
         label=[r"$g_{L}(t)$", r"$g_{R}(t)$"], 
         xlabel=r"Tempo ($ms$)", 
         ylabel="Amplitude", 
-        xlim=(-15, 15), 
+        xlim=(-5, 5), 
     ).plot()
 
     ImpulseResponsePlot(
@@ -271,14 +306,13 @@ if __name__ == "__main__":
         label=r"$g(t)$", 
         xlabel=r"Tempo ($ms$)", 
         ylabel="Amplitude", 
-        xlim=(-15, 15), 
+        xlim=(-5, 5), 
         amp_norm=True
     ).plot()
 
     fig_impulse_man.tight_layout()
     save_figure(fig_impulse_man, "example_formatter_impulse_man.pdf")
 
-    # Plotando os sinais formatados
     fig_format, grid_format = create_figure(3, 2, figsize=(16, 12))
 
     ImpulseResponsePlot(
@@ -289,7 +323,7 @@ if __name__ == "__main__":
         label=r"$g(t)$", 
         xlabel=r"Tempo ($ms$)", 
         ylabel="Amplitude", 
-        xlim=(-15, 15), 
+        xlim=(-5, 5), 
         amp_norm=True
     ).plot()
 
@@ -301,7 +335,7 @@ if __name__ == "__main__":
         label=r"$g(t)$", 
         xlabel=r"Tempo ($ms$)", 
         ylabel="Amplitude", 
-        xlim=(-15, 15), 
+        xlim=(-5, 5), 
         amp_norm=True
     ).plot()
 
