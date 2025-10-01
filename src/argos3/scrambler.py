9d15af8849d4f99b79e1cef4e247cbe4d90a21c0
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-10-01 13:16:05 -0300

    :sparkles: updated code on main

--- src/argos3/scrambler.py
+++ src/argos3/scrambler.py
@@ -14,9 +14,68 @@ from .plotter import save_figure, create_figure, BitsPlot
 class Scrambler:
     def __init__(self):
         r"""
-        Inicializa o embaralhador no padrão ARGOS-3.
+        Scramble the vectors $v_t^{(0)}$ and $v_t^{(1)}$, returning the vectors $X_n$ and $Y_n$ scrambled. The scrambling process is given by the expression below.
 
-        Example: 
+        \begin{equation}
+            X_n = \begin{cases}
+            A, & \text{if } n = 0 \pmod{3} \\
+            B, & \text{if } n = 1 \pmod{3} \\
+            C, & \text{if } n = 2 \pmod{3}
+            \end{cases} \quad
+            Y_n = \begin{cases}
+            A, & \text{if } n = 0 \pmod{3} \\
+            B, & \text{if } n = 1 \pmod{3} \\
+            C, & \text{if } n = 2 \pmod{3}
+            \end{cases}
+        \end{equation}
+
+        Where: 
+            - $X_n$ and $Y_n$: Scrambled output vectors.
+            - $A$, $B$ and $C$: Combination of bits of the input vectors $v_t^{(0)}$ and $v_t^{(1)}$.
+            - $n$: Index of the bit to be scrambled.
+
+        The scrambling process is illustrated by the block diagram below. 
+
+        ![pageplot](../assets/scrambler.svg)
+        
+
+        Examples: 
+            >>> import argos3
+            >>> import numpy as np
+            >>> 
+            >>> vt0 = np.random.randint(0, 2, 15)
+            >>> vt1 = np.random.randint(0, 2, 15)
+            >>> 
+            >>> idx_vt0 = [f"X{i+1}" for i in range(len(vt0))]
+            >>> idx_vt1 = [f"Y{i+1}" for i in range(len(vt1))]
+            >>> 
+            >>> scrambler = argos3.Scrambler()
+            >>> Xn, Yn = scrambler.scramble(vt0, vt1)
+            >>> idx_Xn, idx_Yn = scrambler.scramble(idx_vt0, idx_vt1)
+            >>> 
+            >>> print(vt0)
+            [0 1 1 1 1 0 0 0 1 0 0 0 0 0 1]
+            >>> print(vt1)
+            [1 0 0 1 1 1 0 1 0 1 0 0 0 1 1]
+            >>> print(idx_vt0)
+            ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'X13', 'X14', 'X15']
+            >>> print(idx_vt1)
+            ['Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8', 'Y9', 'Y10', 'Y11', 'Y12', 'Y13', 'Y14', 'Y15']
+            >>> 
+            >>> print(idx_Xn)
+            ['Y1', 'X2', 'Y2', 'Y4', 'X5', 'Y5', 'Y7', 'X8', 'Y8', 'Y10', 'X11', 'Y11', 'Y13', 'X14', 'Y14']
+            >>> print(idx_Yn)
+            ['X1', 'X3', 'Y3', 'X4', 'X6', 'Y6', 'X7', 'X9', 'Y9', 'X10', 'X12', 'Y12', 'X13', 'X15', 'Y15']
+            >>>
+            >>> vt0_prime, vt1_prime = scrambler.descramble(Xn,Yn)
+            >>> idx_vt0_prime, idx_vt1_prime = scrambler.descramble(idx_Xn, idx_Yn)
+            >>> 
+            >>> print(idx_vt0_prime)
+            ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'X13', 'X14', 'X15']
+            >>> print(idx_vt1_prime)
+            ['Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8', 'Y9', 'Y10', 'Y11', 'Y12', 'Y13', 'Y14', 'Y15']
+             
+            
             ![pageplot](assets/example_scrambler_time.svg)
 
         <div class="referencia">
@@ -28,42 +87,20 @@ class Scrambler:
 
     def scramble(self, vt0, vt1):
         r"""
-        Embaralha os vetores $v_t^{(0)}$ e $v_t^{(1)}$, retornando os vetores $X_n$ e $Y_n$ embaralhados. O processo de embaralhamento é dado pela expressão abaixo.
+        Receive the vectors $v_t^{(0)}$ and $v_t^{(1)}$ as input and return the vectors $X_n$ and $Y_n$ scrambled.
 
-        \begin{equation}
-            X_n = \begin{cases}
-            A, & \text{se } n = 0 \pmod{3} \\
-            B, & \text{se } n = 1 \pmod{3} \\
-            C, & \text{se } n = 2 \pmod{3}
-            \end{cases} \quad
-            Y_n = \begin{cases}
-            A, & \text{se } n = 0 \pmod{3} \\
-            B, & \text{se } n = 1 \pmod{3} \\
-            C, & \text{se } n = 2 \pmod{3}
-            \end{cases}
-        \end{equation}
-
-        Sendo: 
-            - $X_n$ e $Y_n$: Vetores de saída embaralhados.
-            - $A$, $B$ e $C$: Combinação de bits dos vetores de entrada $v_t^{(0)}$ e $v_t^{(1)}$.
-            - $n$: Indice do bit a ser embaralhado.
-
-        O processo de embaralhamento é ilustrado pelo diagrama de blocos abaixo. 
-
-        ![pageplot](../assets/scrambler.svg)
-        
         Args:
-            vt0 (np.ndarray): Vetor de entrada $v_t^{(0)}$.
-            vt1 (np.ndarray): Vetor de entrada $v_t^{(1)}$.
+            vt0 (np.ndarray): Input vector $v_t^{(0)}$.
+            vt1 (np.ndarray): Input vector $v_t^{(1)}$.
 
         Returns:
-            X_scrambled (np.ndarray): Vetor $X_n$ embaralhado.
-            Y_scrambled (np.ndarray): Vetor $Y_n$ embaralhado.
+            X_scrambled (np.ndarray): Scrambled vector $X_n$.
+            Y_scrambled (np.ndarray): Scrambled vector $Y_n$.
 
         Raises:
-            AssertionError: Se os vetores X e Y não tiverem o mesmo comprimento.
+            AssertionError: If the vectors X and Y do not have the same length.
         """
-        assert len(vt0) == len(vt1), "Vetores X e Y devem ter o mesmo comprimento"
+        assert len(vt0) == len(vt1), "Vectors X and Y must have the same length"
         X_scrambled = []
         Y_scrambled = []
 
@@ -73,19 +110,19 @@ class Scrambler:
             n = len(x_blk)
 
             if n == 3:
-                # Embaralhamento do bloco [x1, x2, x3], [y1, y2, y3]
+                # Scrambling of the block [x1, x2, x3], [y1, y2, y3]
                 x1, x2, x3 = x_blk
                 y1, y2, y3 = y_blk
                 X_scrambled += [y1, x2, y2]
                 Y_scrambled += [x1, x3, y3]
             elif n == 2:
-                # Embaralhamento do bloco [x1, x2], [y1, y2]
+                # Scrambling of the block [x1, x2], [y1, y2]
                 x1, x2 = x_blk
                 y1, y2 = y_blk
                 X_scrambled += [y1, x2]
                 Y_scrambled += [x1, y2]
             elif n == 1:
-                # Embaralhamento do bloco [x1], [y1]
+                # Scrambling of the block [x1], [y1]
                 x1 = x_blk[0]
                 y1 = y_blk[0]
                 X_scrambled += [y1]
@@ -95,44 +132,43 @@ class Scrambler:
 
     def descramble(self, X_prime, Y_prime):
         r"""
-        Desembaralha os vetores $X'_n$ e $Y'_n$ embaralhados, retornando os vetores $v_t^{(0)'}$ e $v_t^{(1)'}$ restaurados. O processo de desembaralhamento
-        é dado pela expressão abaixo.
+        Receive the vectors $X'_n$ and $Y'_n$ scrambled and return the vectors $v_t^{(0)'}$ and $v_t^{(1)'}$ restored.
 
         \begin{equation}
             v_t^{(0)'} = \begin{cases}
-            A, & \text{se } n = 0 \pmod{3} \\
-            B, & \text{se } n = 1 \pmod{3} \\
-            C, & \text{se } n = 2 \pmod{3}
+            A, & \text{if } n = 0 \pmod{3} \\
+            B, & \text{if } n = 1 \pmod{3} \\
+            C, & \text{if } n = 2 \pmod{3}
             \end{cases}, \quad
             v_t^{(1)'} = \begin{cases}
-            A, & \text{se } n = 0 \pmod{3} \\
-            B, & \text{se } n = 1 \pmod{3} \\
-            C, & \text{se } n = 2 \pmod{3}
+            A, & \text{if } n = 0 \pmod{3} \\
+            B, & \text{if } n = 1 \pmod{3} \\
+            C, & \text{if } n = 2 \pmod{3}
             \end{cases} \text{ .}
             \label{eq:desembaralhador_Y}
         \end{equation}
 
-        Sendo: 
-            - $v_t^{(0)'}$ e $v_t^{(1)'}$: Vetores de saida desembaralhados.
-            - $A$, $B$ e $C$: Combinação de bits dos vetores de entrada $X'_n$ e $Y'_n$ embaralhados.
-            - $n$: Indice do bit a ser embaralhado.
+        Where: 
+            - $v_t^{(0)'}$ and $v_t^{(1)'}$: Output vectors restored.
+            - $A$, $B$ and $C$: Combination of bits of the input vectors $X'_n$ and $Y'_n$ scrambled.
+            - $n$: Index of the bit to be scrambled.
 
-        O processo de desembaralhamento é ilustrado pelo diagrama de blocos abaixo.
+        The descrambling process is illustrated by the block diagram below.
 
         ![pageplot](../assets/unscrewer.svg)
 
         Args:
-            X_prime (np.ndarray): Vetor $X'_{n}$ embaralhado.
-            Y_prime (np.ndarray): Vetor $Y'_{n}$ embaralhado.
+            X_prime (np.ndarray): Vector $X'_{n}$ scrambled.
+            Y_prime (np.ndarray): Vector $Y'_{n}$ scrambled.
 
         Returns:
-            vt0_prime (np.ndarray): Vetor $v_t^{(0)}$ restaurado.
-            vt1_prime (np.ndarray): Vetor $v_t^{(1)}$ restaurado.
+            vt0_prime (np.ndarray): Vector $v_t^{(0)}$ restored.
+            vt1_prime (np.ndarray): Vector $v_t^{(1)}$ restored.
         
         Raises:
-            AssertionError: Se os vetores X e Y não tiverem o mesmo comprimento.
+            AssertionError: If the vectors X and Y do not have the same length.
         """
-        assert len(X_prime) == len(Y_prime), "Vetores X e Y devem ter o mesmo comprimento"
+        assert len(X_prime) == len(Y_prime), "Vectors X and Y must have the same length"
         vt0_prime = []
         vt1_prime = []
 
@@ -142,19 +178,19 @@ class Scrambler:
             n = len(x_blk)
 
             if n == 3:
-                # Desembaralhamento do bloco [y1, x2, y2], [x1, x3, y3]
+                # Descrambling of the block [y1, x2, y2], [x1, x3, y3]
                 x1, x2, x3 = y_blk[0], x_blk[1], y_blk[1]
                 y1, y2, y3 = x_blk[0], x_blk[2], y_blk[2]
                 vt0_prime.extend([x1, x2, x3])
                 vt1_prime.extend([y1, y2, y3])
             elif n == 2:
-                # Desembaralhamento do bloco [y1, x2], [x1, y2]
+                # Descrambling of the block [y1, x2], [x1, y2]
                 x1, x2 = y_blk[0], x_blk[1]
                 y1, y2 = x_blk[0], y_blk[1]
                 vt0_prime.extend([x1, x2])
                 vt1_prime.extend([y1, y2])
             elif n == 1:
-                # Desembaralhamento do bloco [y1], [x1]
+                # Descrambling of the block [y1], [x1]
                 x1 = y_blk[0]
                 y1 = x_blk[0]
                 vt0_prime.append(x1)
@@ -170,28 +206,28 @@ if __name__ == "__main__":
     idx_vt0 = [f"X{i+1}" for i in range(len(vt0))]
     idx_vt1 = [f"Y{i+1}" for i in range(len(vt1))]
 
-    # Embaralha o conteúdo dos vetores e os indices
+    # Scramble the content of the vectors and the indices
     scrambler = Scrambler()
     Xn, Yn = scrambler.scramble(vt0, vt1)
     idx_Xn, idx_Yn = scrambler.scramble(idx_vt0, idx_vt1)
 
-    print("\nSequência original:")
+    print("\nOriginal sequence:")
     print("vt0: ", ''.join(str(b) for b in vt0))
     print("vt1: ", ''.join(str(b) for b in vt1))
     print("idx_vt0:", idx_vt0[:12])
     print("idx_vt1:", idx_vt1[:12])
 
-    print("\nSequência embaralhada:")
+    print("\nScrambled sequence:")
     print("Xn  :", ''.join(str(int(b)) for b in Xn))
     print("Yn  :", ''.join(str(int(b)) for b in Yn))
     print("idx_Xn: ", idx_Xn[:12])
     print("idx_Yn: ", idx_Yn[:12])
 
-    # Desembaralha o conteúdo dos vetores e os indices
+    # Descramble the content of the vectors and the indices
     vt0_prime, vt1_prime = scrambler.descramble(Xn, Yn)
     idx_vt0_prime, idx_vt1_prime = scrambler.descramble(idx_Xn, idx_Yn)
 
-    print("\nVerificação:")
+    print("\nVerification:")
     print("vt0':", ''.join(str(int(b)) for b in vt0_prime))
     print("vt1':", ''.join(str(int(b)) for b in vt1_prime))
     print("idx_vt0': ", idx_vt0_prime[:12])
