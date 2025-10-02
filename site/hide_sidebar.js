503cf1cf32ac4cab422f02d4101dc589efc5d462
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-10-01 23:21:29 -0300

    Deployed 497eb8932 with MkDocs version: 1.6.1

--- site/hide_sidebar.js
+++ site/hide_sidebar.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (isDesktop) {
         // Procura o link ativo do menu
         const activeLink = document.querySelector(".md-nav__link--active");
-        if (activeLink && activeLink.textContent.trim() === "Início") {
+        if (activeLink && activeLink.textContent.trim() === "Home") {
             // Oculta a sidebar
             const sidebar = document.querySelector(".md-sidebar--primary[data-md-type='navigation']");
             if (sidebar) sidebar.style.display = "none";
